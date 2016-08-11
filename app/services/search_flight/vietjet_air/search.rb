4da1a009185f4caac59b820e97d74dba7c9e71bd
Tang Nguyen <nvtang.khtn@gmail.com>
2016-08-11 15:46:26 +0700

    vietjet air searching part 1

--- app/services/search_flight/vietjet_air/search.rb
+++ app/services/search_flight/vietjet_air/search.rb
@@ -2,7 +2,8 @@ module SearchFlight
   module VietjetAir
     class Search
       include HTTParty
-      base_uri "http://book.vietjetair.com"
+      base_uri "https://book.vietjetair.com"
+
       attr_accessor :params, :response
 
       def initialize params
@@ -10,77 +11,86 @@ module SearchFlight
       end
 
       def call
-        first_response = self.class.post "/ameliapost.aspx?lang=vi", build_first_options
-        byebug
-        @response = self.class.post "/ameliapost.aspx?lang=vi", build_second_options(first_response)
-        byebug
+        response = self.class.post "/ameliapost.aspx?lang=vi", build_first_options
+        response = self.class.post "/ameliapost.aspx?lang=vi", build_second_options(response)
+
+
+        # agent = Mechanize.new
+        # first_response = agent.post "https://book.vietjetair.com/ameliapost.aspx?lang=vi", build_first_options[:body], build_first_options[:headers]
+        File.open("out.html", "wb") do |f|
+          f.write first_response.body
+        end
+        # byebug
+        # byebug
+        # @response = agent.post "/ameliapost.aspx?lang=vi", build_second_options(first_response)
+        # byebug
       end
 
       def build_first_options
         {
           body: first_options,
           headers: {
-            "Host": "book.vietjetair.com",
-            "Accept-Encoding": "gzip, deflate",
-            "Content-Type": "application/x-www-form-urlencoded"
+            "Host" => "book.vietjetair.com",
+            "Accept-Encoding" => "gzip, deflate",
+            "Content-Type" => "application/x-www-form-urlencoded"
           }
         }
       end
 
       def first_options
         {
-          "txtPromoCode": "",
-          "lstDepDateRange": "0",
-          "lstRetDateRange": "0",
-          "lstLvlService": "1",
-          "blnFares": "False",
-          "lstCurrency": "VND",
-          "lstResCurrency": "VND",
-          "chkRoundTrip": get_round_type,
-          "txtNumInfants": params[:infant],
-          "txtNumChildren": params[:child],
-          "txtNumAdults": params[:adult],
-          "dlstDepDate_Day": format_day(params[:depart_date]),
-          "dlstRetDate_Day": format_day(round_trip? ? params[:return_date] : params[:depart_date]),
-          "dlstRetDate_Month": format_month(params[:depart_date]),
-          "dlstDepDate_Month": format_month(round_trip? ? params[:return_date] : params[:depart_date]),
-          "lstDestAP": params[:des_code],
-          "lstOrigAP": params[:ori_code]
+          "txtPromoCode" => "",
+          "lstDepDateRange" => "0",
+          "lstRetDateRange" => "0",
+          "lstLvlService" => "1",
+          "blnFares" => "False",
+          "lstCurrency" => "VND",
+          "lstResCurrency" => "VND",
+          "chkRoundTrip" => get_round_type,
+          "txtNumInfants" => params[:infant],
+          "txtNumChildren" => params[:child],
+          "txtNumAdults" => params[:adult],
+          "dlstDepDate_Day" => format_day(params[:depart_date]),
+          "dlstRetDate_Day" => format_day(round_trip? ? params[:return_date] : params[:depart_date]),
+          "dlstRetDate_Month" => format_month(params[:depart_date]),
+          "dlstDepDate_Month" => format_month(round_trip? ? params[:return_date] : params[:depart_date]),
+          "lstDestAP" => params[:des_code],
+          "lstOrigAP" => params[:ori_code]
         }
       end
 
       def build_second_options(response)
-        options = {
+        {
           body: second_options,
           headers: {
-            "Cookie": response.headers["set-cookie"],
-            "Accept-Encoding": "gzip, deflate",
-            "Content-Type": "application/x-www-form-urlencoded"
+            # "Cookie" => response.headers["set-cookie"],
+            "Accept-Encoding" => "gzip, deflate",
+            "Content-Type" => "application/x-www-form-urlencoded"
           }
         }
       end
 
       def second_options
         {
-          "__VIEWSTATE": "/wEPDwULLTE1MzQ1MjI3MzAPZBYCZg9kFg4CCA8QZGQWAGQCCQ8QZGQWAGQCCw8QZGQWAGQCDQ8QZGQWAGQCEQ8QZGQWAGQCEg8QZGQWAGQCEw8QZGQWAGRk/SLp6eYBboDTdTTmIOra109LSis=",
-          "txtPromoCode": "",
-          "lstDepDateRange": "0",
-          "lstRetDateRange": "0",
-          "lstLvlService": "1",
-          "lstCurrency": "VND",
-          "lstResCurrency": "VND",
-          "txtNumInfants": "0",
-          "txtNumChildren": "0",
-          "txtNumAdults": "0",
-          "lstOrigAP": "-1",
-          "lstDestAP": "-1",
-          "__VIEWSTATEGENERATOR": "35449566",
-          "SesID": "",
-          "DebugID": round_trip? ? "43" : "06",
-          "dlstRetDate_Day": format_day(Date.today),
-          "dlstDepDate_Day": format_day(Date.today),
-          "dlstDepDate_Month": format_month(Date.today),
-          "dlstRetDate_Month": format_month(Date.today)
+          "__VIEWSTATE" => "/wEPDwULLTE1MzQ1MjI3MzAPZBYCZg9kFg4CCA8QZGQWAGQCCQ8QZGQWAGQCCw8QZGQWAGQCDQ8QZGQWAGQCEQ8QZGQWAGQCEg8QZGQWAGQCEw8QZGQWAGRk/SLp6eYBboDTdTTmIOra109LSis=",
+          "txtPromoCode" => "",
+          "lstDepDateRange" => "0",
+          "lstRetDateRange" => "0",
+          "lstLvlService" => "1",
+          "lstCurrency" => "VND",
+          "lstResCurrency" => "VND",
+          "txtNumInfants" => "0",
+          "txtNumChildren" => "0",
+          "txtNumAdults" => "0",
+          "lstOrigAP" => "-1",
+          "lstDestAP" => "-1",
+          "__VIEWSTATEGENERATOR" => "35449566",
+          "SesID" => "",
+          "DebugID" => round_trip? ? "43" : "06",
+          "dlstRetDate_Day" => format_day(Date.today),
+          "dlstDepDate_Day" => format_day(Date.today),
+          "dlstDepDate_Month" => format_month(Date.today),
+          "dlstRetDate_Month" => format_month(Date.today)
         }
       end
 
--- app/services/search_flight/vietnam_airlines/search.rb
+++ app/services/search_flight/vietnam_airlines/search.rb
@@ -1,5 +1,3 @@
-require "mechanize"
-
 module SearchFlight
   module VietnamAirlines
     class Search
