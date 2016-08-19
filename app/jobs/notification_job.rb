d1325ee467f4bcc7fe7a492ddc22800970f0c9d2
Hong Tran <phihong2204@gmail.com>
2016-08-19 21:38:06 +0700

    update alert

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -10,17 +10,16 @@ class NotificationJob < ApplicationJob
   private
   def search_tickets(alert)
   	# call craw data ticket airline (ori_airport_id, des_airport_id, time_start) in here
-    @alert = alert
     #@flight_search = SearchFlight::Base.new(ori_code: "SGN", des_code: "PQC", depart_date: Date.today + 1, adult: 1, child: 0, infant: 0, return_date: Date.today + 2, round_type: "OW")
-    @flight_search = SearchFlight::Base.new(ori_code: Airport.find(@alert.ori_air_id).code, des_code: Airport.find(@alert.des_air_id).code, depart_date: @alert.time_start, adult: 1, child: 0, infant: 0, round_type: "OW")
-    @flights = @flight_search.call
-    if @flights[:depart_flights].count > 0
-      @flights_order = @flights[:depart_flights].sort_by{|ob| ob[:price_adult]}
+    flight_search = SearchFlight::Base.new(ori_code: Airport.find(alert.ori_air_id).code, des_code: Airport.find(alert.des_air_id).code, depart_date: alert.time_start, adult: 1, child: 0, infant: 0, round_type: "OW")
+    flights = flight_search.call
+    if flights[:depart_flights].count > 0
+      flights_order = flights[:depart_flights].sort_by{|ob| ob[:price_adult]}
     	#check condition price in here
       #if condition is oke then send email to user
-      @ticket_cheapest = @flights_order.first
-      if @alert.price_expect.to_i >= @ticket_cheapest[:price_adult]
-        UserMailer.notification_email(@alert).deliver_now
+      ticket_cheapest = flights_order.first
+      if alert.price_expect.to_i >= ticket_cheapest[:price_adult]
+        UserMailer.notification_email(alert).deliver_now
       end
     end
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,12 +54,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: "in-v3.mailjet.com",
-    port: 587,
-    authentication: "plain",
-    user_name: ENV["MAILJET_USERNAME"],
-    password: ENV["MAILJET_PASSWORD"]
+    ActionMailer::Base.smtp_settings = {
+    :user_name => 'iFlight',
+    :password => 'iflight12345',
+    :domain => 'https://iflight.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
