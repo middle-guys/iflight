5745c213f4343bcb089f00c867c77c38e42ea606
Tang Nguyen <nvtang.khtn@gmail.com>
2016-08-20 09:38:56 +0700

    remove rspec

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,16 +1,16 @@
-### Test for creating an account
+# ### Test for creating an account
 
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "Spec for Sign Up" do     
-     it "should create new user account" do     
-	visit "/users/sign_up"  
-	email = "abcd@example.com" 
-	fill_in 'user_name', :with => 'abcdtesttest'   
-	fill_in 'user_email', :with => email    
-	fill_in 'user_password', :with => "password"    
-	fill_in 'user_phone', :with => '0988034925'    
-	click_button 'Sign up'    
-	expect(page).to have_content "Please follow the link to activate your account"    
-    end    
-end
+# describe "Spec for Sign Up" do
+#      it "should create new user account" do
+# 	visit "/users/sign_up"
+# 	email = "abcd@example.com"
+# 	fill_in 'user_name', :with => 'abcdtesttest'
+# 	fill_in 'user_email', :with => email
+# 	fill_in 'user_password', :with => "password"
+# 	fill_in 'user_phone', :with => '0988034925'
+# 	click_button 'Sign up'
+# 	expect(page).to have_content "Please follow the link to activate your account"
+#     end
+# end
--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,19 +1,19 @@
-require "spec_helper"
+# require "spec_helper"
 
-describe "user sign in" do
-  it "allows users to sign in after they have registered" do
-    user = User.new(:email    => "alindeman@example.com",
-                       :password => "ilovegrapes")
-    user.skip_confirmation!
-    user.save! validate: false
+# describe "user sign in" do
+#   it "allows users to sign in after they have registered" do
+#     user = User.new(:email    => "alindeman@example.com",
+#                        :password => "ilovegrapes")
+#     user.skip_confirmation!
+#     user.save! validate: false
 
-    visit "/users/sign_in"
+#     visit "/users/sign_in"
 
-    fill_in "Email",    :with => "alindeman@example.com"
-    fill_in "Password", :with => "ilovegrapes"
+#     fill_in "Email",    :with => "alindeman@example.com"
+#     fill_in "Password", :with => "ilovegrapes"
 
-    click_button "Log in"
+#     click_button "Log in"
 
-    page.should have_content("Signed in successfully.")
-  end
-end
+#     page.should have_content("Signed in successfully.")
+#   end
+# end
