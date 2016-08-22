1f934cbe494fff8f2d7452519f7c36786a10f03a
Leo Le <coder.leo.le@gmail.com>
2016-08-23 06:08:31 +0700

    add booking code to receipt email

--- spec/mailers/previews/order_mailer_preview.rb
+++ spec/mailers/previews/order_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
   def receipt
-    OrderMailer.receipt()
+    OrderMailer.receipt(Order.last)
   end
 end
